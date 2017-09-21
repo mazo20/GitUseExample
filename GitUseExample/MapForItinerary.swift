412dbff5f649d7b4ae2afe4617ac4a7cc30f3ea0
mazu20 <maciek789@gmail.com>
2017-09-21 14:23:43 +0100

    Added map file

---
+++ GitUseExample/MapForItinerary.swift
@@ -0,0 +1,9 @@
+//
+//  MapForItinerary.swift
+//  GitUseExample
+//
+//  Created by Maciej Kowalski on 21.09.2017.
+//  Copyright © 2017 Maciej Kowalski. All rights reserved.
+//
+
+import Foundation
